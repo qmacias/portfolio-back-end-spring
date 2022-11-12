e4c34e92a5a4ca97452314f33a2d823410de886f
qmacias <ezequielqmacias@gmail.com>
2022-11-12 09:54:48 -0300

    Add Basic Jpa Crud Implementation

--- portfolio-infrastructure/src/main/java/org/example/portfolio/infrastructure/adapter/ImageJpaAdapter.java
+++ portfolio-infrastructure/src/main/java/org/example/portfolio/infrastructure/adapter/ImageJpaAdapter.java
@@ -1,10 +1,15 @@
 package org.example.portfolio.infrastructure.adapter;
 
+import org.example.portfolio.domain.data.PersonDto;
+import org.example.portfolio.infrastructure.entity.ImageEntity;
+import org.example.portfolio.infrastructure.entity.PersonEntity;
+import org.example.portfolio.infrastructure.mapper.Mapper;
 import org.example.portfolio.infrastructure.repository.ImageRepository;
 import org.example.portfolio.domain.data.ImageDto;
 import org.example.portfolio.domain.port.out.ImagePersistencePort;
 
 import java.util.List;
+import java.util.Optional;
 
 public class ImageJpaAdapter implements ImagePersistencePort {
 
@@ -21,16 +26,29 @@ public class ImageJpaAdapter implements ImagePersistencePort {
 
     @Override
     public ImageDto findById(Long id) {
+
+        Optional<ImageEntity> imageEntity = imageRepository.findById(id);
+
+        if (imageEntity.isPresent()) {
+            return Mapper.INSTANCE.map(imageEntity, ImageDto.class);
+        }
+
         return null;
     }
 
     @Override
     public ImageDto save(ImageDto imageDto) {
-        return null;
+
+        ImageEntity imageEntity = Mapper.INSTANCE.map(imageDto, ImageEntity.class);
+
+        ImageEntity imageEntitySaved = imageRepository.save(imageEntity);
+
+        return Mapper.INSTANCE.map(imageEntitySaved, ImageDto.class);
     }
 
     @Override
     public void deleteById(Long id) {
+        imageRepository.deleteById(id);
     }
 
 }
--- portfolio-infrastructure/src/main/java/org/example/portfolio/infrastructure/adapter/PersonJpaAdapter.java
+++ portfolio-infrastructure/src/main/java/org/example/portfolio/infrastructure/adapter/PersonJpaAdapter.java
@@ -1,11 +1,16 @@
 package org.example.portfolio.infrastructure.adapter;
 
+import org.example.portfolio.domain.data.ImageDto;
 import org.example.portfolio.domain.data.PersonDto;
 import org.example.portfolio.domain.port.out.PersonPersistencePort;
 
+import org.example.portfolio.infrastructure.entity.ImageEntity;
+import org.example.portfolio.infrastructure.entity.PersonEntity;
+import org.example.portfolio.infrastructure.mapper.Mapper;
 import org.example.portfolio.infrastructure.repository.PersonRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public class PersonJpaAdapter implements PersonPersistencePort {
 
@@ -22,16 +27,29 @@ public class PersonJpaAdapter implements PersonPersistencePort {
 
     @Override
     public PersonDto findById(Long id) {
+
+        Optional<PersonEntity> personEntity = personRepository.findById(id);
+
+        if (personEntity.isPresent()) {
+            return Mapper.INSTANCE.map(personEntity, PersonDto.class);
+        }
+
         return null;
     }
 
     @Override
     public PersonDto save(PersonDto personDto) {
-        return null;
+
+        PersonEntity personEntity = Mapper.INSTANCE.map(personDto, PersonEntity.class);
+
+        PersonEntity personEntitySaved = personRepository.save(personEntity);
+
+        return Mapper.INSTANCE.map(personEntitySaved, PersonDto.class);
     }
 
     @Override
     public void deleteById(Long id) {
+        personRepository.deleteById(id);
     }
 
 }
