838d3d655eeae013c487b8494465358c9e503699
qmacias <ezequielqmacias@gmail.com>
2022-12-17 09:21:38 -0300

    Remove Unnecessary Ports, Repositories And Adapters

--- portfolio-domain/src/main/java/org/example/portfolio/domain/port/output/AchievementPersistencePort.java
+++
@@ -1,7 +0,0 @@
-package org.example.portfolio.domain.port.output;
-
-import org.example.portfolio.domain.model.AchievementDto;
-import org.example.portfolio.domain.common.GenericPersistencePort;
-
-public interface AchievementPersistencePort extends GenericPersistencePort<AchievementDto, String> {
-}
